3129e6b3ae387537b240cabcff31216f8b66fb1e
José Puma <45923235+josepuma@users.noreply.github.com>
2024-02-16 12:23:34 -0300

    Initial Commit

---
+++ storyboard-editor-front/storyboard_editor_frontApp.swift
@@ -0,0 +1,17 @@
+//
+//  storyboard_editor_frontApp.swift
+//  storyboard-editor-front
+//
+//  Created by José Puma on 16-02-24.
+//
+
+import SwiftUI
+
+@main
+struct storyboard_editor_frontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
